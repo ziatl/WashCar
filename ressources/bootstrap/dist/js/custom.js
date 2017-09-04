b9a2189946276eec0150e96face66531d916ae17
Liloudini Aziz <zizoa1er@gmail.com>
2017-09-04 12:11:29 +0200

    Mise au point des margins

--- ressources/bootstrap/dist/js/custom.js
+++ ressources/bootstrap/dist/js/custom.js
@@ -48,36 +48,7 @@ jQuery(document).ready(function () {
     }
 
 
-    //this code is for animation nav
-    jQuery(window).scroll(function () {
-        var windowScrollPosTop = jQuery(window).scrollTop();
 
-        if (windowScrollPosTop >= 150) {
-            jQuery(".header").css({
-                "background": "#B193DD"
-            });
-            jQuery(".top-header img.logo").css({
-                "margin-top": "-40px",
-                "margin-bottom": "0"
-            });
-            jQuery(".navbar-default").css({
-                "margin-top": "-15px"
-            });
-        } else {
-            jQuery(".header").css({
-                "background": "transparent"
-            });
-            jQuery(".top-header img.logo").css({
-                "margin-top": "-15px",
-                "margin-bottom": "25px"
-            });
-            jQuery(".navbar-default").css({
-                "margin-top": "12px",
-                "margin-bottom": "0"
-            });
-
-        }
-    });
 
 
 
